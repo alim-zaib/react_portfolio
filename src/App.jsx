44f407d47a9db5332107586a6dcba44acf7fe00c
alim-zaib <alimtwiz@gmail.com>
2024-09-15 20:23:28 +0100

    fixing mobile viewport issues

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ const App = () => {
         <div className="absolute top-0 z-[-2] w-screen min-h-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] bg-cover"></div>
       </div>
       <div className="mx-auto px-4 lg:px-20">
-        {" "}
         <Navbar />
         <Hero />
         <About />

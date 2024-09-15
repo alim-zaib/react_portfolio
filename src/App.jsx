e126988ea1db63c22db8fb961890c436c378b1ca
alim-zaib <alimtwiz@gmail.com>
2024-09-15 20:38:22 +0100

    removed bg snippet to fix iphone issues

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,9 @@ import Contact from "./components/Contact";
 
 const App = () => {
   return (
-    <div className="min-h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 w-full h-full">
-        {" "}
-        {/* Keep fixed but ensure height is full */}
-        <div className="absolute top-0 z-[-2] w-full h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] bg-cover"></div>
-      </div>
+    <div className="min-h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 bg-neutral-950">
+      {/* Removed the background snippet, added solid background colour */}
       <div className="relative mx-auto px-4 lg:px-20 min-h-screen">
-        {" "}
-        {/* Make content full height */}
         <Navbar />
         <Hero />
         <About />
